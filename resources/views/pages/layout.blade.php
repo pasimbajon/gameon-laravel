9a0c7ee4e95057865f7bc31c95dbcb71c6e2fb00
Paul Arnold Simbajon <40814631+pasimbajon@users.noreply.github.com>
2023-05-29 21:37:01 +0800

    Second commit

--- resources/views/pages/layout.blade.php
+++ resources/views/pages/layout.blade.php
@@ -25,6 +25,17 @@
                 <li><a class='links' href="/">STORE</a></li>
                 <li><a class='links' href="/discover">DISCOVER</a></li>
                 <li><a class='links' href="/about">ABOUT</a></li>
+                <li>
+                  @if (Route::has('login'))
+                    <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
+                      @auth
+                          <a class="links" href="{{ url('/dashboard') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
+                      @else
+                          <a class="links" href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</a>
+                      @endauth
+                    </div>
+                  @endif
+                </li>
             </ul>
         </div>
         <div class='toggle-button'>
